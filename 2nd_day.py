8a3bffa1939505b2fab383932fcca52d7b158582
mitrovicbojan <79321143+mitrovicbojan@users.noreply.github.com>
2025-10-29 22:03:59 +0200

    2nd day

--- 2nd_day.py
+++ 2nd_day.py
@@ -17,3 +17,41 @@ def convert_add(lst):
 
 result = convert_add(["1","3","5"])
 print(result)
+
+'''
+Write a function called check_duplicates that takes a list of
+strings as an argument. The function should check if the list has
+any duplicates. If there are duplicates, the function should
+return a list of duplicates. If there are no duplicates, the
+function should return "no duplicates.
+" For example, the list of
+fruits below should return ["apple","banana"], and the list
+of names should return "no duplicates."
+fruits = ['apple', 'orange', 'banana', 'apple', 'banana'] names =
+['Yoda', 'Moses', 'Joshua', 'Mark']
+
+'''
+fruits = ['apple', 'orange', 'banana', 'apple', 'banana']
+names = ['Yoda', 'Moses', 'Joshua', 'Mark']
+
+def check_duplicates(lst):
+    
+    unique = set()
+    duplicates = set()
+    
+    for i in lst: 
+        if i in unique:
+            duplicates.add(i)
+        else:
+            unique.add(i)
+    
+    if not duplicates:
+        return "No duplicates."
+    else: 
+        dbls = list(duplicates)
+        return dbls
+    
+    
+print(check_duplicates(names))
+
+
---
+++ 3rd_day.py
@@ -0,0 +1,25 @@
+'''
+Write a function called register_check that checks how many
+students are in school. The function takes a dictionary as an
+argument. If the student is in school, the dictionary says "yes." 
+If the student is not in school, the dictionary says "no." Your function
+should return the number of students in school. Using the dictionary
+below, your function should return 3.
+
+register = {'Michael': 'yes', 'John': 'no', 'Peter': 'yes', 'Mary': 'yes'}
+'''
+def register_check():
+    print("hello")
+
+'''
+names = ["kerry", "dickson", "John", "Mary","carol","Rose","adam"]
+
+You are given a list of names above. This list is made up of names
+win lowercase and uppercase letters. Your task is to write a code
+that will return a tuple of all the names in the list that only have
+lowercase letters. Your tuple should have names sorted
+alphabetically in descending order. Using the list above, your code
+should return:
+('kerry','dickson','carol','adam')
+
+'''
