e19c1f162acc7a5116d5deaef5526a5bbd0711a8
mitrovicbojan <79321143+mitrovicbojan@users.noreply.github.com>
2025-10-29 21:26:39 +0200

    convert list os str to int

--- 2nd_day.py
+++ 2nd_day.py
@@ -1,7 +1,19 @@
 '''
 Day 2: Strings to Integers
-Write afunction called convert_add that takes a list of strings
+Write a function called convert_add that takes a list of strings
 as an argument, converts them to integers, and sums the list. For
 example, ["1","3","5"] should be converted to [1, 3, 5] and
 summed to 9.
 '''
+
+def convert_add(lst):
+    int_list = []
+    
+    for i in lst:
+        int_list.append(int(i))
+        
+    return sum(int_list)
+   
+
+result = convert_add(["1","3","5"])
+print(result)
