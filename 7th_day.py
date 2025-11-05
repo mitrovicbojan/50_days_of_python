1a1ea6427cc281c6d473c0024408caab0e8d25f7
mitrovicbojan <79321143+mitrovicbojan@users.noreply.github.com>
2025-11-05 21:49:37 +0200

    7th day

--- 7th_day.py
+++ 7th_day.py
@@ -18,11 +18,27 @@ string_range(6)
 
 '''
 You are given a list of names, and you are asked to write a code that
-returns all the names that start with "S.
-" Your code should return
+returns all the names that start with "S." Your code should return
 a dictionary of all the names that start with S and how many times
 they appear in the dictionary. Here is a list below:
 names = ["Joseph", "Nathan", "Sasha", "Kelly","Muhammad", "Jabulani", "Sera", "Patel", "Sera"]
 Your code should return: {"Sasha": 1,"Sera": 2}
 
 '''
+
+names = ["Joseph", "Nathan", "Sasha", "Kelly","Muhammad", "Jabulani", "Sera", "Patel", "Sera"]
+
+s_names = []
+for name in names:
+    if name[0] == 'S':
+        s_names.append(name)
+
+
+s_names_dict = {name: s_names.count(name) for name in s_names}
+print(s_names_dict)
+
+# different way
+
+name_dict = {name: names.count(name) for name in names if name[0]== "S"}
+
+print(name_dict)
