90cf684de639383e8cbc85f56fd29e4f2074a33d
mitrovicbojan <79321143+mitrovicbojan@users.noreply.github.com>
2025-11-08 21:59:50 +0200

    12th

--- 11th_day.py
+++ 11th_day.py
@@ -8,3 +8,24 @@ return False. For example,
 True.
 
 '''
+def equal_strings(str1, str2):
+    
+    return sorted(str1) == sorted(str2)
+
+print(equal_strings("love", "evol"))
+
+'''
+Write a function called swap_values. This function takes a
+list of numbers and swaps the first element with the last
+element. For example, if you pass [2, 4, 67, 7] as a
+parameter, your function should return.
+[7, 4, 67, 2].
+'''
+def swap_values(lst):
+    last = lst.pop()
+    first = lst.pop(0)
+            
+    return [last] + lst + [first]
+
+print(swap_values([2, 4, 67, 7]))
+
