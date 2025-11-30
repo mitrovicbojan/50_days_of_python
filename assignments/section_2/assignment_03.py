c2fe5dcf6bb769024be2c238da608a31f84b1cff
mitrovicbojan <79321143+mitrovicbojan@users.noreply.github.com>
2025-11-30 11:02:19 +0200

    s2

---
+++ assignments/section_2/assignment_03.py
@@ -0,0 +1,14 @@
+# Assignment 3:
+"""
+There is a list shown below titled original_list.
+
+Using only what you've learned so far in the course,
+write code to create a new list that contains the tuple sorted.
+
+IMPORTANT: you must do this programmatically! Don't just
+      hard code a new list with the values rearranged.
+"""
+
+original_list = ['cup', 'cereal', 'milk', (8, 4, 3)]
+
+# your code below:
