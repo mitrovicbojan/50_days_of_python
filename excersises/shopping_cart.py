01e7c79a1bc224e31ab6f27247ab510bdba8acec
mitrovicbojan <79321143+mitrovicbojan@users.noreply.github.com>
2026-01-18 14:45:34 +0200

    shopping_cart

--- excersises/shopping_cart.py
+++ excersises/shopping_cart.py
@@ -15,3 +15,36 @@ total() → returns total price of items.
 
 __str__() → prints items in format: "Item: <name>, Price: <price>" and total.
 '''
+
+class ShoppingCart:
+    def __init__(self, owner, items):
+        self.owner = owner
+        self.items = items
+    
+    def add_item(self,name, price):
+        self.items.append({
+            "name": name,
+            "price": price
+        })
+    
+    def remove_item(self, name):
+        for item in self.items:
+            if item['name'] == name:
+                self.items.remove(item)
+                break
+    
+    def total(self):
+        cart_prices = []
+        for item in self.items:
+            cart_prices.append(item['price'])
+            
+        cart_total = sum(cart_prices)    
+        return cart_total
+        
+    def __str__(self):
+        total = f"Total: {self.total()}"
+        total_bill = ""
+        for item in self.items:
+            total_bill += f"Item: {item['name']}, Price: {item['price']}\n"
+            
+        return total_bill + total
