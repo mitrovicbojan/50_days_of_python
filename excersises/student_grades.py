66de0046ec8d8bfa7e7aad7e56c45e919b060235
mitrovicbojan <79321143+mitrovicbojan@users.noreply.github.com>
2026-01-18 13:32:29 +0200

    grades

--- excersises/student_grades.py
+++ excersises/student_grades.py
@@ -13,5 +13,30 @@ average() → returns the average grade (rounded to 2 decimals).
 
 __str__() → "Student <name>, Average: <average>".
 
-Bonus: Make a class method class_average(students_list) that calculates the average grade for a list of Student objects.
 '''
+
+class Student:
+    def __init__(self, name, grades=None):
+        self.name = name
+        self.grades = grades if grades is not None else []
+        self._average = self._calculate_average()
+                
+    def add_grade(self, grade):
+        if not isinstance(grade, (int, float)):
+            raise ValueError("Please enter valid numeric grades.")
+        self.grades.append(grade)
+        self._average = self._calculate_average()
+    
+    def _calculate_average(self):
+        if not self.grade:
+            return 0
+        sum_grades = sum(self.grades)
+        avg = round(sum_grades / len(self.grades), 2)
+        return avg
+    
+    @property
+    def average(self):
+        return self._average
+    
+    def __str__(self):
+        return f"Student: {self.name}\nAverage: {self.average}"
