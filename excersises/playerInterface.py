9929b1ae6cdfa471e05b563c3632f8ae0f41bf4b
mitrovicbojan <79321143+mitrovicbojan@users.noreply.github.com>
2026-02-08 11:31:53 +0200

    interface

--- excersises/playerInterface.py
+++ excersises/playerInterface.py
@@ -35,7 +35,28 @@ using aliases. Tests related to the Player class will fail until
 the Pawn class becomes instantiable. 
 '''
 from abc import ABC, abstractmethod
+import random
 
 class Player(ABC):
     def __init__(self):
+        self.moves = []
+        self.position = (0,0)
+        self.path = [self.position]
+    
+    def make_move(self):
+        rnd_move = random.choice(self.moves)
+        x_new = self.position[0] + rnd_move[0]
+        y_new = self.position[1] + rnd_move[1]
+        self.position = (x_new, y_new)
+        self.path.append(self.position)
+        return self.position
+    
+    @abstractmethod
+    def level_up(self):
         pass
+class Pawn(Player):
+    def __init__(self):
+        super().__init__()
+        self.moves = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+    def level_up(self):
+        self.moves.extend([(1, 1), (1, -1), (-1, 1), (-1, -1)])
